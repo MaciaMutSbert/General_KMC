e046529dec87e29fbd77ce2bce4813a412756ce0
maciamutsbert <macia.mut.sbert@gmail.com>
2019-08-18 20:05:31 +0200

    kmc_algorithm --> commented. definitive file

--- core/kmc_implementation.py
+++ core/kmc_implementation.py
@@ -8,6 +8,9 @@ def kmc_algorithm(rate_list, process_list):
     :param process_list: List of elements dict(center, process, new molecule).
     The indexes of each rate in rate_list have the same index that the associated process in
     process_list.
+    Chooses a process using the list of rates and associates a time with this process using
+    the BKL Kinetic Monte-Carlo algorithm. The algorithm uses 2 random number, one to choose the process and the other
+    for the time. The usage of each random number is in an independent function
     :return:    plan: The chosen proces and the new molecule affected
                 time: the duration of the process
     """
@@ -22,17 +25,24 @@ def kmc_algorithm(rate_list, process_list):
 def select_process(constant_list):
     """
     :param constant_list: List with the constant rates
-    :return: Chooses a position of the list proportionally to its value.
+    :return: Chooses a position of the list chosen proportionally to its value.
     """
     r = np.sum(constant_list) * rd.random()
+    # random number picked from the uniform distribution U(0, rates sum)
+
     list_ = np.where(r > np.cumsum(constant_list))
+    # the first element of list returned by np.where is a list with the positions of the list of accumulative sums of
+    # the rates where the desired condition is satisfied.
+
+    # The length of these first list is the position of the chosen process (the last position
+    # where the condition is satisfied)
     return len(list_[0])
 
 
 def time_advance(rate_list):
     """
     :param rate_list: List with all the rates. Considering all the processes for all exciton
-    :return: Process duration
+    :return: Process duration. Picks a random time from an exponential distribution
     """
     r = rd.random()
     return (-np.log(r)) / (np.sum(rate_list))
